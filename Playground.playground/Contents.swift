33d281697d5e2cd4ec0f9189c772728ef10fc1c8
nevillco <connor.neville@raizlabs.com>
2018-05-26 23:12:49 -0400

    Drop in example code from AutoEquatable

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-print(type(of: SourceryProtocol.self))
+func valueList(for enumCase: EnumCase, suffix: String) -> String {
+    let list = enumCase.associatedValues
+        .map({ value in
+            guard let name = value.localName else {
+                fatalError("AutoEquatable enums should have associated types with labels")
+            }
+            return "\(name)\(suffix)"
+        })
+        .joined(separator: ", ")
+    return enumCase.associatedValues.isEmpty ? "" : "(\(list))"
+}
+
+func caseDeclaration(for enumCase: EnumCase) -> String {
+    let caseName = enumCase.name
+    let lhsList = valueList(for: enumCase, suffix: "LHS")
+    let rhsList = valueList(for: enumCase, suffix: "RHS")
+    let optionalLet = enumCase.associatedValues.isEmpty ? "" : "let"
+    return "case \(optionalLet) (.\(caseName)\(lhsList), .\(caseName)\(rhsList)):"
+}
