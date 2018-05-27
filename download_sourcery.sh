8d78eb2ba71a050e22ed4b25b5c0fbd6d934bd4d
nevillco <connor.neville@raizlabs.com>
2018-05-26 22:23:34 -0400

    Clean up script

--- download_sourcery.sh
+++ download_sourcery.sh
@@ -13,7 +13,7 @@ fi
 SOURCERY_DIR="./Sourcery"
 if [ -d "$SOURCERY_DIR" ] ; then
     echo "Removing old Sourcery directory..."
-    rm -rf ./Sourcery
+    rm -rf "$SOURCERY_DIR"
 fi
 
 bundle exec git clone https://github.com/krzysztofzablocki/Sourcery $SOURCERY_DIR
@@ -21,8 +21,8 @@ bundle exec git clone https://github.com/krzysztofzablocki/Sourcery $SOURCERY_DI
 RUNTIME_SOURCE_DIR="$SOURCERY_DIR/SourceryRuntime/Sources/"
 RUNTIME_TARGET_DIR="./SourceryRuntime/"
 echo "Copying SourceryRuntime directory to root..."
-if [ ! -d "$RUNTIME_TARGET_DIR" ] ; then
-    mkdir "$RUNTIME_TARGET_DIR"
+if [ -d "$RUNTIME_TARGET_DIR" ] ; then
+    rm -rf "$RUNTIME_TARGET_DIR"
 fi
 cp -R "$RUNTIME_SOURCE_DIR" "$RUNTIME_TARGET_DIR"
 
