578361ea53731633d6d191759718573fe1e35f39
nevillco <connor.neville@raizlabs.com>
2018-05-26 21:41:35 -0400

    Add commands to remove Sourcery directory if it exists, and clone to directory

--- download_sourcery.sh
+++ download_sourcery.sh
@@ -1,2 +1,9 @@
+echo "Installing any dependencies..."
 gem install bundler
 bundle install
+
+echo "Removing old Sourcery directory..."
+rm -rf ./Sourcery
+
+echo "Cloning Sourcery..."
+bundle exec git clone https://github.com/krzysztofzablocki/Sourcery
